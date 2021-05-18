b17b88638d8b14f858aa190d2175b9f3843979c2
Balazs Kovacs <balazskovacs48@gmail.com>
2021-05-18 19:53:56 +0300

    Ex3

--- c2-samples/src/main/java/aut/utcluj/isp/ex3/test.java
+++ c2-samples/src/main/java/aut/utcluj/isp/ex3/test.java
@@ -1,21 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package aut.utcluj.isp.ex3;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  *
- * @author Asus
+ * @author KBalazs
  */
 public class test {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Equipment firstEquipment = new Equipment("Dell", "SN123", "John");
+        Equipment secondEquipment = new Equipment("HP", "SN124", "John");
+        Equipment thirdEquipment = new Equipment("Toshiba", "SN125", "John");
+        
+        EquipmentController equipmentController = new EquipmentController();
+        
+        equipmentController.addEquipment(firstEquipment);
+        equipmentController.addEquipment(secondEquipment);
+        equipmentController.addEquipment(thirdEquipment);
+        
+        System.out.println("\nThere is "+equipmentController.getNumberOfEquipments()+" equpments.\n");
+    
+        List<Equipment> equipmentList = equipmentController.getEquipments();
+        System.out.println("The first equipment name is "+equipmentList.get(0).getName());
+        
+        Map<String, List<Equipment>> equipmentsGroupedByOwner = equipmentController.getEquipmentsGroupedByOwner();
+        List<Equipment> johnEquipments = equipmentsGroupedByOwner.get("John");
+        System.out.println("\nThis equipments are at John:\n");
+        for (Equipment elements: johnEquipments){
+            System.out.println(elements.getName()+"\n");
+        }
+        
+        System.out.println("After deleting "+equipmentController.removeEquipmentBySerialNumber("SN123").getName()+" there is "+equipmentController.getNumberOfEquipments()+" equipments.");
+    
     }
     
 }
